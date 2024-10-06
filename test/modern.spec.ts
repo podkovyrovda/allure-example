a55c55ce947e48cb7f94af727ab6504e16cc8f23
Andrei Podkovyrov <a.podkovyrov@gmail.com>
2024-10-06 12:27:13 +0100

    initial commit

---
+++ test/modern.spec.ts
@@ -0,0 +1,21 @@
+import {expect, it} from "vitest";
+import * as allure from "allure-js-commons";
+
+it("sample test", async () => {
+  await allure.owner("John Doe x");
+  await allure.issue("https://example.org", "JIRA-2");
+  await allure.step("step 1", async () => {
+    await allure.step("step 1.2", async () => {
+      await allure.attachment("text attachment", "some data", "text/plain");
+    });
+  });
+  await allure.step("step 2", async () => {
+  });
+});
+
+it("flaky test", async () => {
+  await allure.owner("Andrei P");
+  await allure.step("should be greater than 0.5", async () => {
+    expect(Math.random()).toBeGreaterThan(0.5)
+  });
+});
